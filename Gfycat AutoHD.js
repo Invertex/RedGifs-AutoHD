0643faa82ca3289b3fcec02574c3697ee4856221
Dabbles in too many things <mitch.zais@gmail.com>
2019-08-16 19:08:06 -0700

    Integrated a require that Greasyfork hasn't approved

--- Gfycat AutoHD.js
+++ Gfycat AutoHD.js
@@ -7,13 +7,27 @@
 // @match https://gfycat.com/*
 // @match https://*.gfycat.com/*
 // @require  https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
-// @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @grant none
 // @run-at document-start
 // ==/UserScript==
 
 const thumbsStr = 'thumbs.gfycat';
 const mobileStr = '-mobile.';
 const settingsButtonClass = ".settings-button";
+
+(function()
+{
+    var url = window.location.href;
+
+    if (url.includes(thumbsStr))
+    {
+        setHDURL(url);
+    }
+    else
+    {
+        waitForKeyElements(settingsButtonClass, settingsClick);
+    }
+})();
 
 function setHDURL(url)
 {
@@ -30,18 +44,80 @@ function settingsClick(settingsButton)
     if(settingsButton) { settingsButton.click() }
 };
 
-(function()
-{
-    var url = window.location.href;
+//Had to directly include waitForKeyElements.js since Greasyfork hasn't approved the include...
+//Following function by https://gist.github.com/BrockA
+function waitForKeyElements (
+    selectorTxt,    /* Required: The jQuery selector string that
+                        specifies the desired element(s).
+                    */
+    actionFunction, /* Required: The code to run when elements are
+                        found. It is passed a jNode to the matched
+                        element.
+                    */
+    bWaitOnce,      /* Optional: If false, will continue to scan for
+                        new elements even after the first match is
+                        found.
+                    */
+    iframeSelector  /* Optional: If set, identifies the iframe to
+                        search.
+                    */
+) {
+    var targetNodes, btargetsFound;
 
-    if (url.includes(thumbsStr))
-    {
-        setHDURL(url);
-    }
+    if (typeof iframeSelector == "undefined")
+        targetNodes     = $(selectorTxt);
     else
-    {
-        waitForKeyElements(settingsButtonClass, settingsClick);
+        targetNodes     = $(iframeSelector).contents ()
+                                           .find (selectorTxt);
+
+    if (targetNodes  &&  targetNodes.length > 0) {
+        btargetsFound   = true;
+        /*--- Found target node(s).  Go through each and act if they
+            are new.
+        */
+        targetNodes.each ( function () {
+            var jThis        = $(this);
+            var alreadyFound = jThis.data ('alreadyFound')  ||  false;
+
+            if (!alreadyFound) {
+                //--- Call the payload function.
+                var cancelFound     = actionFunction (jThis);
+                if (cancelFound)
+                    btargetsFound   = false;
+                else
+                    jThis.data ('alreadyFound', true);
+            }
+        } );
     }
-})();
+    else {
+        btargetsFound   = false;
+    }
 
+    //--- Get the timer-control variable for this selector.
+    var controlObj      = waitForKeyElements.controlObj  ||  {};
+    var controlKey      = selectorTxt.replace (/[^\w]/g, "_");
+    var timeControl     = controlObj [controlKey];
 
+    //--- Now set or clear the timer as appropriate.
+    if (btargetsFound  &&  bWaitOnce  &&  timeControl) {
+        //--- The only condition where we need to clear the timer.
+        clearInterval (timeControl);
+        delete controlObj [controlKey]
+    }
+    else {
+        //--- Set a timer, if needed.
+        if ( ! timeControl) {
+            timeControl = setInterval ( function () {
+                    waitForKeyElements (    selectorTxt,
+                                            actionFunction,
+                                            bWaitOnce,
+                                            iframeSelector
+                                        );
+                },
+                300
+            );
+            controlObj [controlKey] = timeControl;
+        }
+    }
+    waitForKeyElements.controlObj   = controlObj;
+}
