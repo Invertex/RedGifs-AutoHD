a88517da48f67e5f79c0da97849b8e4c1d8a8f09
Invertex <Invertex@users.noreply.github.com>
2021-11-06 15:08:10 -0700

    Better fix for play button glitch that isn't so timing focused

--- Gfycat AutoHD.user.js
+++ Gfycat AutoHD.user.js
@@ -7,7 +7,7 @@
 // @license GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
 // @homepageURL https://github.com/Invertex/Gfycat-AutoHD
 // @supportURL https://github.com/Invertex/Gfycat-AutoHD
-// @version 1.73
+// @version 1.74
 // @match *://*.gifdeliverynetwork.com/*
 // @match *://cdn.embedly.com/widgets/media.html?src=*://*.gfycat.com/*
 // @match *://cdn.embedly.com/widgets/media.html?src=*://*.redgifs.com/*
@@ -292,13 +292,16 @@ async function removeMobileQualityVideos(container, video)
 
     if(video == null) { console.log("no vid"); return; }
 
-    await sleep(140); //For some reason redgifs button clicking stops working if we do this change too soon...
-
     let controls = container.querySelector('.player-controls');
+    let playBtn = controls.querySelector('.play-buttons > button');
+    playBtn.click(); //Have to pause, change quality, wait and then unpause to avoid RedGifs button events becoming glitched
 
     let settingsButton = controls.querySelector('div.right > span.settings-button, div.options-buttons > .has-badge');
     let qualityIcon = settingsButton.querySelector('.quality, .icon-badge');
     changeQualitySettings(settingsButton, qualityIcon);
+
+    await sleep(100);
+    playBtn.click();
 };
 
 function hideElem(elem)
